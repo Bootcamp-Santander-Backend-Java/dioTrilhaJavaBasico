3efc4951fc7c4eac343665cc8994caaf59a9c10a
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2024-05-02 18:44:44 -0300

    adicionando detalhes nos cometarios TODO

--- ContaBanco/src/edu/paulo/dio/banco/ContaTerminal.java
+++ ContaBanco/src/edu/paulo/dio/banco/ContaTerminal.java
@@ -12,9 +12,9 @@ import java.util.Scanner;
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		// Importando a classe scaner e locale
+		//TODO Importar a classe scaner e locale
 		try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
-			// exibir mensagens solicitando dados
+			//TODO exibir mensagens solicitando dados
 			System.out.println("Digite o Número da conta : ");
 			int numeroConta = scanner.nextInt();
 			System.out.println("Digite o numero da Agencia");
@@ -22,6 +22,7 @@ public class ContaTerminal {
 			System.out.println("Digite seu nome: ");
 			String nome = scanner.next();
 			float saldo = (float) 245.68;
+			//TODO imprimir informaçõe na tela
 			System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
 					+ " Sua conta é " + numeroConta + " e seu saldo de R$" + saldo + " já esta disponível");
 		}
